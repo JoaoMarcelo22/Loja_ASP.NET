80e06aac5ae81cf9a93d5f6a44c70676bb2f1923
joao marcelo <joao.ferreira@nodis.com.br>
2022-05-30 18:00:51 -0300

    feat: :sparkles: Edições

--- LojaAsp/Controllers/LojaController.cs
+++ LojaAsp/Controllers/LojaController.cs
@@ -1,3 +1,4 @@
+using LojaAsp.Data;
 using LojaAsp.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,32 +8,64 @@ namespace LojaAsp.Controllers
     [Route("[controller]")]
     public class LojaController : ControllerBase
     {
-        private static List<Produto> produtos = new List<Produto>();
-        private static int id = 1; 
+        private ProdutoContext _context;
+
+        public LojaController(ProdutoContext context)
+        {
+            _context = context;
+        }   
 
         [HttpPost]
         public IActionResult AdicionarProduto([FromBody]Produto produto)
         {
-            produto.Id = id++;
-            produtos.Add(produto);
+            _context.Produtos.Add(produto);
+            _context.SaveChanges();   
             return CreatedAtAction(nameof(BuscarProdutoId), new { Id = produto.Id}, produto);
         }
 
         [HttpGet]
-        public IActionResult BuscarProduto()
+        public IEnumerable<Produto> BuscarProduto()
         {
-            return Ok(produtos);
+            return _context.Produtos;
         }
 
         [HttpGet("{id}")]
         public IActionResult BuscarProdutoId(int id)
         {
-            Produto produto = produtos.FirstOrDefault(produto => produto.Id == id);
+            Produto produto = _context.Produtos.FirstOrDefault(produto => produto.Id == id);
             if(produto != null)
             {
                 return Ok(produto);
             }
             return NotFound();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult EditarProduto(int id, [FromBody]Produto produtoEditado)
+        {
+            Produto produto = _context.Produtos.FirstOrDefault(produto => produto.Id == id);
+            if(produto == null)
+            {
+                NoContent();
+            }
+            produto.Nome = produtoEditado.Nome;
+            produto.Categoria = produtoEditado.Categoria;
+            produto.Preco = produtoEditado.Preco;
+            _context.SaveChanges();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeletarProduto(int id)
+        {
+            Produto produto = _context.Produtos.FirstOrDefault(produto => produto.Id == id);
+            if(produto == null )
+            {
+                NoContent();
+            }
+            _context.Remove(produto);
+            _context.SaveChanges();
+            return NoContent();
         }
     }
 }
